5384d4105821480a380b01cb495909522a29c3ae
Samantha Coyle <sam@diagrid.io>
2024-01-18 11:34:42 -0600

    feat: add daprd grpc middleware
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -35,6 +35,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	gogrpc "google.golang.org/grpc"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/clock"
 
@@ -142,6 +143,9 @@ type DaprRuntime struct {
 	workflowEngine *wfengine.WorkflowEngine
 
 	wg sync.WaitGroup
+
+	// diagrid custom
+	customMiddlewareProvider func() (gogrpc.UnaryServerInterceptor, gogrpc.StreamServerInterceptor)
 }
 
 // newDaprRuntime returns a new runtime with the given runtime config and global config.
@@ -351,6 +355,14 @@ func newDaprRuntime(ctx context.Context,
 	}
 
 	return rt, nil
+}
+
+func (a *DaprRuntime) GetGlobalConfig() *config.Configuration {
+	return a.globalConfig
+}
+
+func (a *DaprRuntime) SetCustomGRPCMiddlewareProvider(c func() (gogrpc.UnaryServerInterceptor, gogrpc.StreamServerInterceptor)) {
+	a.customMiddlewareProvider = c
 }
 
 // Run performs initialization of the runtime with the runtime and global configurations.
@@ -976,6 +988,10 @@ func (a *DaprRuntime) startGRPCAPIServer(api grpc.API, port int) error {
 		WorkflowEngine: a.workflowEngine,
 		Healthz:        a.runtimeConfig.healthz,
 	})
+
+	// diagrid custom
+	server.SetCustomMiddlewareProvider(a.customMiddlewareProvider)
+
 	if err := server.StartNonBlocking(); err != nil {
 		return err
 	}
