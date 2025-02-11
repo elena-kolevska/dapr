293ad881df50a5ab8cf9164efa73e45ffb33b854
Elena Kolevska <elena@kolevska.com>
2025-02-11 11:30:25 +0000

    Adds nodes in order
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- pkg/scheduler/server/internal/etcd/etcd.go
+++ pkg/scheduler/server/internal/etcd/etcd.go
@@ -15,10 +15,15 @@ package etcd
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/server/v3/embed"
 
@@ -29,6 +34,10 @@ import (
 )
 
 var log = logger.NewLogger("dapr.scheduler.server.etcd")
+
+const NODE0 = "dapr-scheduler-server-0"
+const NODE1 = "dapr-scheduler-server-1"
+const NODE2 = "dapr-scheduler-server-2"
 
 type Options struct {
 	Name                string
@@ -82,17 +91,64 @@ func (e *etcd) Run(ctx context.Context) error {
 	log.Info("Starting etcd")
 
 	var err error
-	e.etcd, err = embed.StartEtcd(e.config)
-	if err != nil {
-		return fmt.Errorf("failed to start embedded etcd: %w", err)
+
+	initialPeers := make(map[string]string, 3)
+	split := strings.Split(e.config.InitialCluster, ",")
+	if len(split) != 3 {
+		return fmt.Errorf("invalid initial cluster: %s", e.config.InitialCluster)
 	}
 
-	e.client, err = clientv3.New(clientv3.Config{
-		Endpoints: []string{e.config.ListenClientUrls[0].Host},
-		Logger:    e.etcd.GetLogger(),
-	})
+	for _, s := range split {
+		nsplit := strings.Split(s, "=")
+		if len(nsplit) != 2 {
+			return fmt.Errorf("invalid initial cluster: %s", e.config.InitialCluster)
+		}
+		initialPeers[nsplit[0]] = nsplit[1]
+	}
+
+	//if e.mode == modes.KubernetesMode {
+	if true {
+		if e.config.Name == NODE0 {
+			e.config.ClusterState = embed.ClusterStateFlagNew
+			e.etcd, err = e.handleKube0(ctx, initialPeers)
+		} else if e.config.Name == NODE1 {
+			fmt.Printf("Starting etcd on %s\n", NODE1)
+
+			e.config.ClusterState = embed.ClusterStateFlagExisting
+			e.config.InitialCluster = fmt.Sprintf("%s=%s,%s=%s", NODE0, initialPeers[NODE0], NODE1, initialPeers[NODE1])
+
+			e.etcd, err = e.startEtcdWithTimeout()
+		} else if e.config.Name == NODE2 {
+			fmt.Printf("Starting etcd on %s\n", NODE2)
+
+			e.config.ClusterState = embed.ClusterStateFlagExisting
+			e.config.InitialCluster = fmt.Sprintf("%s=%s,%s=%s,%s=%s", NODE0, initialPeers[NODE0], NODE1, initialPeers[NODE1], NODE2, initialPeers[NODE2])
+
+			e.etcd, err = e.startEtcdWithTimeout()
+		} else {
+			return fmt.Errorf("invalid node name: %s", e.config.Name)
+		}
+		if err != nil {
+			return fmt.Errorf("error starting etcd: %w", err)
+		}
+
+	} else {
+		e.etcd, err = embed.StartEtcd(e.config)
+		if err != nil {
+			return fmt.Errorf("failed to start etcd: %w", err)
+		}
+	}
+
+	//e.client, err = clientv3.New(clientv3.Config{
+	//	Endpoints: []string{e.config.ListenClientUrls[0].Host},
+	//	Logger:    e.etcd.GetLogger(),
+	//})
+	//if err != nil {
+	//	return errors.Join(err, e.client.Close())
+	//}
+	e.client, err = e.getClientWithTimeout()
 	if err != nil {
-		return errors.Join(err, e.client.Close())
+		return err
 	}
 
 	select {
@@ -113,6 +169,138 @@ func (e *etcd) Run(ctx context.Context) error {
 	}
 }
 
+func (e *etcd) startEtcdWithTimeout() (*embed.Etcd, error) {
+	t := time.NewTimer(30 * time.Second)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-t.C:
+			return nil, fmt.Errorf("********* failed to start etcd within 30 seconds")
+		default:
+			fmt.Println("********* Starting etcd with", e.config.InitialCluster)
+			etcd, err := embed.StartEtcd(e.config)
+			if err == nil {
+				fmt.Println("********* Etcd started successfully!", e.config.InitialCluster)
+				return etcd, nil
+			}
+
+			fmt.Printf("********* Failed to start etcd: %v, retrying...\n", err)
+
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func (e *etcd) getClientWithTimeout() (*clientv3.Client, error) {
+	t := time.NewTimer(30 * time.Second)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-t.C:
+			return nil, fmt.Errorf("********* failed to get etcd client within 30 seconds")
+		default:
+			client, err := clientv3.New(clientv3.Config{
+				Endpoints: []string{e.config.ListenClientUrls[0].Host},
+				Logger:    e.etcd.GetLogger(),
+			})
+			if err == nil {
+				fmt.Printf("***** Client connected to %s\n", e.config.ListenClientUrls[0].Host)
+				return client, nil
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func (e *etcd) handleKube0(ctx context.Context, initialPeers map[string]string) (*embed.Etcd, error) {
+	existingCluster := filepath.Join(e.config.Dir, "dapr-existing-cluster")
+	_, err := os.Stat(existingCluster)
+	if err == nil {
+		log.Infof("Found existing cluster at %s", existingCluster)
+		return nil, nil
+	}
+
+	if !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	e.config.InitialCluster = fmt.Sprintf("%s=%s", NODE0, initialPeers[NODE0])
+
+	etcd, err := embed.StartEtcd(e.config)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{e.config.ListenClientUrls[0].Host},
+		Logger:    etcd.GetLogger(),
+	})
+	if err != nil {
+		return nil, errors.Join(err, e.client.Close())
+	}
+
+	//members, err := client.MemberList(ctx)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//if len(members.Members) > 1 {
+	//	fmt.Println("******* Cluster already exists, skipping member add")
+	//	return nil, os.WriteFile(existingCluster, nil, 0o600)
+	//}
+
+	// Add node 2
+	if _, err := client.MemberAdd(ctx, []string{initialPeers[NODE1]}); err != nil {
+		// TODO check if error is about node already being a member and return if it is
+		return nil, err
+	}
+
+	fmt.Println("******* Added second node to cluster")
+
+	// Wait until Node 2 joins and is ready
+	if err := backoff.Retry(func() error {
+		members, err := client.MemberList(ctx)
+		if err != nil {
+			return err
+		}
+
+		vd, _ := json.MarshalIndent(members, "", "  ")
+		fmt.Println("******* cluster members: ")
+		fmt.Println(string(vd))
+
+		if len(members.Members) != 2 {
+			fmt.Printf("******* Cluster not ready yet, retrying... (%d members)", len(members.Members))
+			return fmt.Errorf("cluster not ready yet")
+		}
+
+		resp, err := client.Status(ctx, members.Members[1].ClientURLs[0])
+		fmt.Printf("*********************** NODE %s STATUS ***********************", members.Members[1].GetName())
+
+		vd, _ = json.MarshalIndent(resp, "", "  ")
+		fmt.Println(string(vd))
+
+		if err != nil {
+			return fmt.Errorf("node %s is unhealthy: %v", members.Members[1].GetName(), err)
+		}
+
+		return nil
+	}, backoff.WithContext(backoff.NewExponentialBackOff(), ctx)); err != nil {
+		log.Errorf("Failed adding second member: %s", err)
+		return nil, err
+	}
+
+	// Add node 3
+	if _, err := client.MemberAdd(ctx, []string{initialPeers[NODE2]}); err != nil {
+		return nil, err
+	}
+	fmt.Println("******* Added node 3 to cluster")
+
+	return etcd, os.WriteFile(existingCluster, nil, 0o600)
+}
+
 func (e *etcd) Client(ctx context.Context) (*clientv3.Client, error) {
 	select {
 	case <-ctx.Done():
@@ -127,11 +315,11 @@ func (e *etcd) Close() error {
 
 	var err error
 	if e.client != nil {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		defer cancel()
-		if _, err = e.client.MemberRemove(ctx, uint64(e.etcd.Server.ID())); err != nil {
-			log.Errorf("Failed to remove member from cluster during shutdown: %s", err)
-		}
+		//ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		//defer cancel()
+		//if _, err = e.client.MemberRemove(ctx, uint64(e.etcd.Server.ID())); err != nil {
+		//	log.Errorf("Failed to remove member from cluster during shutdown: %s", err)
+		//}
 		err = e.client.Close()
 	}
 
